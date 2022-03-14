c8e140905531bd0dce79540d0325cf3255d618ce
Christopher Sexton <csexton@users.noreply.github.com>
2022-03-14 11:59:45 -0400

    Fix eager loading of WildApricot class

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "#{Rails.root}/app/lib/omniauth/strategies/wildapricot"
+require "#{Rails.root}/app/lib/omniauth/strategies/wild_apricot"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :wildapricot, ENV['WA_OAUTH_ID'], ENV['WA_OAUTH_SECRET'], :callback_path => "/callback/wildapricot"
