2f1dd0e63f0bdd3c2aa5a4f1a990e3971b5ba295
Christopher Sexton <csexton@users.noreply.github.com>
2022-03-30 10:42:37 -0400

    Set timezone for Rails
    Nova Labs happens to be on the east coast, so lets set the timezone
    there, this saves us the extra work for saving timezones for each event
    or user.
    
    Considered setting the `timezone` on the event row for each event, but
    then I would also have to convert other dates (like when a user was
    updated), so figured the one-line change is worth a go. May never have
    to revisit!

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module MakerSpace
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Eastern Time (US & Canada)"
   end
 end
