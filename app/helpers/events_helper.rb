35c5c5f10d98819f1166fcc715334d98b2237e8b
Christopher Sexton <csexton@users.noreply.github.com>
2022-06-10 12:53:22 -0400

    Display room name for location
    If there is a room it will be in parens by convention.  So something
    like this:
    
    Nova Labs, 3850 Jermantown Road, Fairfax, VA (South Door Classroom)
    
    -or-
    
    Nova Labs (Classroom 2), 3850 Jermantown Rd, Fairfax VA
    
    So, for displaying the location just show the room. If we don't find a
    match show the entire location string.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def display_location(location)
+    # If there is a room it will be in parens by convention.
+    # So something like this:
+    #
+    # Nova Labs, 3850 Jermantown Road, Fairfax, VA (South Door Classroom)
+    # Nova Labs (Classroom 2), 3850 Jermantown Rd, Fairfax VA
+    #
+    # So, for displaying the locaiton just show the room. If we don't find a
+    # match show the entire location string.
+
+    location[/\((.*)\)/, 1] || location
+  end
 end
