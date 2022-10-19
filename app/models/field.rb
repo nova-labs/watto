ce0341153a50c96448568a72ac9a137be7b000ac
Christopher Sexton <csexton@users.noreply.github.com>
2022-10-19 12:09:54 -0400

    Add sort to signoff fields
    They were named with the specific intention to sort by name. So lets
    sort by name.

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,5 @@
 class Field < ApplicationRecord
-  has_many :allowed_values, class_name: "FieldAllowedValue", dependent: :destroy
+  has_many :allowed_values, -> { order(label: :asc) }, class_name: "FieldAllowedValue", dependent: :destroy
 
   # Boy do I hate using a display name string for a scope
   scope :signoffs, -> { find_by(field_name: "NL Signoffs and Categories") }
