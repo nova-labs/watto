06223465e00f8686ff0b8a91468d582897b08f75
John F <jtommyok8@gmail.com>
2022-11-20 12:06:43 -0500

    batch controller submits appropriate signoffs for classes

--- app/controllers/batch_update_tools_controller.rb
+++ app/controllers/batch_update_tools_controller.rb
@@ -22,24 +22,37 @@ class BatchUpdateToolsController < ApplicationController
     classes_file = File.open("app/assets/data/classes.json")
     classes_data = classes_file.read
     @classes = JSON.parse(classes_data)
+    puts "found the following classes"
+    puts @classes
 
     @contacts = params["contacts"].uniq
     @contacts.each do |uid|
       user = User.find_by uid: uid
       values = user.signoff_values
+      puts "user starts with these values"
+      puts values
     # check if the user submitted a class or a single sign off
-    puts params["field_value"]
-    if params["field_value"].match(/\[class\]/)
-      redirect_to "/", notice: "class found"
-      puts "class found"
-    else
-      puts "class not found"
-      redirect_to batch_update_tool_path
-    end
-    return
-    
-      values << params["field_value"]
-
+      puts params["field_value"]
+      if params["field_value"].match(/\[class\]/)
+        puts "class found"
+        puts "class name is " + params["field_value"]
+        puts "searching for class"
+        @classes.each do |aClass|
+          if aClass["class_name"] == params["field_value"]
+            puts "found class " + aClass["class_name"]
+            puts "with signoffs"
+            puts aClass["signoffs_granted"]
+            #found the right class in our data. Add all of its signoffs to values, so they'll get sent to WA
+            #It looks like WA can handle duplicate signoffs just fine, so no need to dedup the array
+            values.concat(aClass["signoffs_granted"])
+          end
+        end
+      else
+        puts "class not found"    
+        values << params["field_value"]
+      end
+      puts "will submit these values"
+      puts values
       ret = WAAPI.update_contact_field(user.uid, @field.system_code, values.map(&:to_i))
 
       if ret.status != 200
