1036295103ce8f0840419e3360938886a774c1fa
John F <jtommyok8@gmail.com>
2022-12-26 14:01:10 -0800

    updates classes object with new signoffs

--- app/controllers/edit_classes_controller.rb
+++ app/controllers/edit_classes_controller.rb
@@ -8,7 +8,19 @@ class EditClassesController < ApplicationController
   end
 
   def update
-    puts params
+    all_classes = JSON.parse(File.read("app/assets/data/classes.json"))
+    signoff_hash = params["signoff"]
+    new_signoff_list = []
+    signoff_hash.each do |key, value| #value just equals 'on'
+      new_signoff_list.append(helpers.desanitize_signoff(key))
+    end
+    puts new_signoff_list
+    all_classes.each do |single_class|
+      if single_class["class_form_value"] == params["class_name"]
+        single_class["signoffs_granted"] = new_signoff_list
+      end
+    end
+    puts all_classes
     redirect_to edit_classes_path, notice: "Successfully did nothing"
   end
 end
