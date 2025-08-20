3d2b189c940230cbf7b274f83257ddd9867ca060
Christopher Sexton <csexton@users.noreply.github.com>
2025-08-20 11:16:13 -0400

    Force sign off classes to the top of the waitlist
    The waitlist page uses the order of the hash to determine how it is
    displayed on the page that list the different class types. If the
    support desk folks reorder the google sheet then project classes might be at
    the top of the page.
    
    The majority of members care about getting on the waitlist for sign off
    classes the most -- or at least that is where they start. This will
    rebuild the hash so that ruby orders the keys with `SIGN_OFF_CLASS`
    first.

--- app/controllers/waitlist_controller.rb
+++ app/controllers/waitlist_controller.rb
@@ -4,5 +4,11 @@ class WaitlistController < ApplicationController
   def index
     @items = WaitlistCourse.new.get_courses
     @groups = @items.group_by { |item| item["event_type"] }
+
+    # Force sign off classes to be the top section on the page
+    if @groups.key?("SIGN_OFF_CLASS")
+      @groups = { "SIGN_OFF_CLASS" => @groups["SIGN_OFF_CLASS"] }
+        .merge(@groups.except("SIGN_OFF_CLASS"))
+    end
   end
 end
