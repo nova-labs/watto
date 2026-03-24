18af8229401f0a1cfd3a05b7cf1d854264cc93ef
Christopher Sexton <csexton@users.noreply.github.com>
2026-03-24 10:40:10 -0400

    Clarify old event search behavior
    Update the events search UI with a clearer "Search old events" label
    and an info tooltip that opens on click and dismisses on outside click.
    We do not want a combined past+future search because Wild Apricot
    includes many defunct/template events, which makes results cluttered
    and ordering across directions messy and cumbersome for users.

--- app/javascript/controllers/bootstrap_controller.js
+++ app/javascript/controllers/bootstrap_controller.js
@@ -1,17 +1,54 @@
-// app/javascript/controllers/tooltip_controller.js
 import { Controller } from "@hotwired/stimulus"
-import * as bootstrap from "bootstrap"
 
-    console.log("ouch")
 export default class extends Controller {
   connect() {
-    this.tooltip = new bootstrap.Tooltip(this.element)
+    const bootstrap = window.bootstrap
+    if (!bootstrap || !bootstrap.Tooltip) {
+      console.warn("bootstrap tooltip not available")
+      return
+    }
+
+    this.tooltip = new bootstrap.Tooltip(this.element, {
+      delay: { show: 0, hide: 0 },
+      trigger: "click",
+    })
+
+    this.isShown = false
+    this._onShown = () => { this.isShown = true }
+    this._onHidden = () => { this.isShown = false }
+    this.element.addEventListener("shown.bs.tooltip", this._onShown)
+    this.element.addEventListener("hidden.bs.tooltip", this._onHidden)
+
+    this._onDocumentClick = (event) => {
+      if (!this.tooltip || !this.isShown) {
+        return
+      }
+      if (this.element.contains(event.target)) {
+        return
+      }
+      this.tooltip.hide()
+    }
+    document.addEventListener("click", this._onDocumentClick)
   }
 
   disconnect() {
     if (this.tooltip) {
       this.tooltip.dispose()
     }
+    if (this._onShown) {
+      this.element.removeEventListener("shown.bs.tooltip", this._onShown)
+    }
+    if (this._onHidden) {
+      this.element.removeEventListener("hidden.bs.tooltip", this._onHidden)
+    }
+    if (this._onDocumentClick) {
+      document.removeEventListener("click", this._onDocumentClick)
+    }
+  }
+
+  logClick(event) {
+    if (this.tooltip) {
+      this.tooltip.toggle()
+    }
   }
 }
-
